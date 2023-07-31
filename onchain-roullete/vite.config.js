3f09544a5a14aa56b51c51c1fc59b8ef9247d498
= <=>
2023-07-31 01:55:38 +0100

    npm build setup

--- onchain-roullete/vite.config.js
+++ onchain-roullete/vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import nodePolyfills from 'rollup-plugin-node-polyfills';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
-
-import nodePolyfills from 'rollup-plugin-node-polyfills';
-rollup({
-  entry: 'main.jsx',
-  plugins: [
-    nodePolyfills()
-  ]
+  plugins: [react(), nodePolyfills()],
 })
